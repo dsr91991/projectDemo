0928e36686202e142b03e51ed4d0e820a1f9f04a
dsr91991 <dsr91991@gmail.com>
2017-02-28 17:45:23 +0530

    i am adding showdetails

---
+++ ShowDetails.java
@@ -0,0 +1,14 @@
+package com.girnarsoft.hospitalManagement.client;
+
+import java.util.Map;
+
+import com.girnarsoft.hospitalManagement.server.Doctor;
+
+public class ShowDetails {
+	
+	public void doctorList(Map<Integer, Doctor> doctorList)
+	{
+		
+	}
+
+}
